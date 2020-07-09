35a6febd28d6a9aa3f934707f766428a13c9a87d
Ale Feltes <afeltes@sodep.com.py>
2020-07-09 18:22:00 -0400

    Agrega conf. de red entre reading y bookstore

--- reading/src/main/java/com/example/circuitbreakerreading/BookService.java
+++ reading/src/main/java/com/example/circuitbreakerreading/BookService.java
@@ -1,30 +1,42 @@
 package com.example.circuitbreakerreading;
 
 import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
 import java.net.URI;
 
 @Service
+@RefreshScope
 public class BookService {
 
     public static final String OTRO_MICROSERVICE_DE_CONSULTA = "http://localhost:8090/recommended";
     private final RestTemplate restTemplate;
+    Logger LOGGER = LoggerFactory.getLogger(BookService.class);
 
-  public BookService(RestTemplate rest) {
-	this.restTemplate = rest;
-  }
+    @Value("${bookstore.url:http://localhost:8090/recommended}")
+    private String bookstoreUrl;
 
-  @HystrixCommand(fallbackMethod = "reliable")
-  public String readingList() {
-	URI uri = URI.create(OTRO_MICROSERVICE_DE_CONSULTA);
+    public BookService(RestTemplate rest) {
+        this.restTemplate = rest;
+    }
 
-	return this.restTemplate.getForObject(uri, String.class);
-  }
+    @HystrixCommand(fallbackMethod = "reliable")
+    public String readingList() {
+        URI uri = URI.create(OTRO_MICROSERVICE_DE_CONSULTA);
+        if (bookstoreUrl != null) {
+            uri = URI.create(bookstoreUrl);
+        }
+        LOGGER.info("Consultando servicio:  {}", uri);
+        return this.restTemplate.getForObject(uri, String.class);
+    }
 
-  public String reliable() {
-	return "Cloud Native Java (O'Reilly)";
-  }
+    public String reliable() {
+        return "Cloud Native Java (O'Reilly)";
+    }
 
 }
