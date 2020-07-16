6ff6012b90e75f54534adce72deb1450f6e8ced2
Ale Feltes <afeltes@sodep.com.py>
2020-07-16 18:49:10 -0400

    Imprime información de la instancia balanceada

--- reading/src/main/java/com/example/circuitbreakerreading/CircuitBreakerReadingApplication.java
+++ reading/src/main/java/com/example/circuitbreakerreading/CircuitBreakerReadingApplication.java
@@ -1,18 +1,21 @@
 package com.example.circuitbreakerreading;
 
+import com.netflix.appinfo.ApplicationInfoManager;
 import com.netflix.discovery.EurekaClient;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Lazy;
-import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
 @EnableCircuitBreaker
@@ -22,9 +25,15 @@ import org.springframework.web.client.RestTemplate;
 @EnableEurekaClient
 public class CircuitBreakerReadingApplication {
 
+    private Logger LOGGER = LoggerFactory.getLogger(CircuitBreakerReadingApplication.class);
+
+
     @Autowired
     @Lazy
     private EurekaClient eurekaClient;
+
+    @Autowired
+    private ApplicationInfoManager applicationInfoManager;
 
     @Value("${spring.application.name}")
     private String appName;
@@ -47,9 +56,10 @@ public class CircuitBreakerReadingApplication {
 
     @RequestMapping(value = "/greeting")
     public String greeting() {
-        System.out.println("Request received on port number " + portNumber);
-        return String.format("Hello from '%s with Port Number %s'!", eurekaClient.getApplication(appName)
-                .getName(), portNumber);
+        LOGGER.info("Request received on port number " + portNumber);
+        return String.format("Server: %s - Hello from '%s with Port Number %s'!",
+                applicationInfoManager.getEurekaInstanceConfig(),
+                eurekaClient.getApplication(appName).getName(), portNumber);
     }
 
     public static void main(String[] args) {
